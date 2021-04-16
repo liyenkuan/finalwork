42dd850a1cf323e364be91632b93e598963ba9f9
liyenkuan <yenkuanli@gmail.com>
2021-04-16 17:20:54 +0800

    Add files via upload

--- final/app.js
+++ final/app.js
@@ -1,8 +1,16 @@
 const apiId = 'kko';
 const productsList = document.querySelector(".card");
 const selectDom = document.querySelector(".selectCar") ;
+const cartList = document.querySelector(".cartsTbody")
+const cartTotal = document.querySelector(".cartTotal")
 let productsData = [];
+let cartsData =[];
 //https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/kko/products
+//https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/kko/carts
+
+
+
+//初始化產品資料
 function init() {
     axios.get(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${apiId}/products`)
         .then(function (response) {
@@ -11,17 +19,32 @@ function init() {
         productsData = response.data.products;
         console.log(productsData);
         cardPut();
-  })
+  });
 };
+
+//初始化購物車資料
+function initcart() {
+    axios.get(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${apiId}/carts`)
+    .then(function (response) {
+     console.log(response);
+     cartsData = response.data.carts;
+     console.log(cartsData);
+     cartput();
+    });
+};
+
+
+
+//-----------------產品列表-------------------------
 function cardPut() {
     let cardHtml = '';
     productsData.forEach(function (item ) {
-        let cardData = ` <li  data-id = ${item.id}>
+        let cardData = ` <li  >
                             <div class="cardImg" >       
                                     <img  src="${item.images}" alt=""/>  
                                 <div class="CardNew">新品</div>
                             </div>
-                            <div class="cardText"> <a href="#">加入購物車</a></div>
+                            <div class="cardText"> <a href="#" class ="addCar" data-id = ${item.id}>加入購物車</a></div>
                             <div class="Pname">${item.title}</div>
                             <div class="Pname" style="text-decoration:line-through;">${item.origin_price}</div>
                             <div class="price">${item.price}</div>
@@ -29,7 +52,34 @@ function cardPut() {
         cardHtml+=cardData;
     });
     productsList.innerHTML =  cardHtml;
+    const list =  document.querySelector(".addCar") ;
+    console.log(list);
 };
+
+
+
+//-----------------購物車-------------------------
+function cartput() {
+    let cartHtml = '';
+    let cartPrice = '';
+    cartsData.forEach(function (item ) {
+        let cartData = `<td>
+        <img src="${item.product.images}">
+        ${item.product.title}</td>
+        <td>${item.product.title}</td> 
+        <td>1</td> 
+        <td>NT$${item.product.price}</td>
+        <td><a href="#" data-id =${item.product.id} ><img style="width: 30px; height: 30px;" src="https://i.imgur.com/FvkjACF.png"></a></td>  
+    </tr>`;
+        cartHtml += cartData;
+        let carttotalprice = Number(item.product.price);
+        cartPrice += carttotalprice ;
+    });
+    cartList.innerHTML = cartHtml;
+    cartTotal.innerHTML = `NT$${cartPrice}`;
+};
+
+//---------------搜選-------------------------
 selectDom.addEventListener("change",function (e) {
     let selectData = productsData;
     console.log(selectData );
@@ -40,12 +90,12 @@ selectDom.addEventListener("change",function (e) {
        let searchData= selectData.filter(search =>search.category ==selectDom.value );
        let cardHtml = '';
        searchData.forEach(function (item) {
-        let cardData = ` <li  data-id = ${item.id}>
+        let cardData = ` <li  >
                         <div class="cardImg" >       
                                 <img  src="${item.images}" alt=""/>  
                             <div class="CardNew">新品</div>
                         </div>
-                        <div class="cardText"> <a href="#">加入購物車</a></div>
+                        <div class="cardText"> <a href="#"   class ="addCar" data-id = ${item.id}>加入購物車</a></div>
                         <div class="Pname">${item.title}</div>
                         <div class="Pname" style="text-decoration:line-through;">${item.origin_price}</div>
                         <div class="price">${item.price}</div>
@@ -56,3 +106,4 @@ selectDom.addEventListener("change",function (e) {
     };
 });
 init();
+initcart();
